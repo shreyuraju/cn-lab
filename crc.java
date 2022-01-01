22c4906dcee3164ff7767e5aae10ffb4afd992fa
Shreyas M <56906523+shreyuraju@users.noreply.github.com>
2022-01-01 12:57:44 +0530

    Update crc.java

--- crc.java
+++ crc.java
@@ -1,69 +1,72 @@
 import java.io.*;
-import java.net.*;
 import java.util.*;
-public class CRC {
-	
+public class bellmanford {
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter data:");
-			String strdata=sc.nextLine();
-		System.out.print("Enter generator polynomial:");
-			String gen=sc.nextLine();
-		String code=strdata;
-		while( code.length() < (strdata.length()+gen.length()-1) ) {
-			code=code+"0";
-		}
-		code=strdata+div(code,gen);
-		System.out.println(code);
-		String rem=div(code,gen);
-		if(Integer.parseInt(rem) == 0) {
-			System.out.println("No error in data");
-		}
-		else {
-			System.out.println("error in data");
-		}
-		System.out.print("Enter position to alter the data:");
-		int pos=sc.nextInt();
-		if( code.charAt(pos) == '0' )
-			code=code.substring(0,pos)+"1"+code.substring(pos+1);
-		else
-			code=code.substring(0,pos)+"0"+code.substring(pos+1);
-		System.out.println("Altered data:"+code);
-		if(Integer.parseInt(div(code,gen)) == 0)
-			System.out.println("No error");
-		else
-			System.out.println("Error");
-	}
-	static String div(String num1,String num2) {
-		int pointer=num2.length();
-		String result=num1.substring(0,pointer);
-		String rem="";
-		for(int i=0;i<pointer;i++) {
-			if(result.charAt(i)==num2.charAt(i))
-				rem=rem+"0";
-			else
-				rem=rem+"1";
-		}
-		while(pointer<num1.length()) {
-			if(rem.charAt(0)=='0') {
-				rem=rem.substring(1,rem.length());
-				rem=rem+String.valueOf(num1.charAt(pointer));
-				pointer++;
-			}
-			result=rem;
-			rem="";
-			if(result.charAt(0)!='0'){
-				for(int i=0;i<num2.length();i++) {
-					if(result.charAt(i) == num2.charAt(i))
-						rem+="0";
-					else
-						rem+="1";
+		System.out.print("Enter no.of vertices:");
+		int nv=sc.nextInt();
+		int adj_matrix[][]=new int[nv+1][nv+1];
+		System.out.println("Enter adjecency matrix:");
+		for(int sn=1;sn<=nv;sn++) {
+			for(int dn=1;dn<=nv;dn++) {
+				adj_matrix[sn][dn]=sc.nextInt();
+				if(sn==dn) {
+					adj_matrix[sn][dn]=0;
+					continue;
+				}
+				if(adj_matrix[sn][dn] == 0) {
+					adj_matrix[sn][dn]=999;
 				}
 			}
-			else {
-				rem=result;
+		}
+		System.out.println("given adjecency matrix:");
+		for(int sn=1;sn<=nv;sn++) {
+			for(int dn=1;dn<=nv;dn++) {
+				System.out.print(adj_matrix[sn][dn]+"\t");
 			}
-		}			
-		return rem.substring(1,rem.length());
+			System.out.print("\n");
+		}
+		System.out.print("Enter source node:");
+			int source=sc.nextInt();
+		bellmanford bf=new bellmanford(nv);
+		bf.bellmanevaluation(source,adj_matrix,nv);
+		sc.close();
 	}
-}		
+	private int nv;
+	private int distance[];
+	public bellmanford(int nv) {
+		this.nv=nv;
+		distance=new int[nv+1];
+	}
+	public void bellmanevaluation(int source,int adj_matrix[][],int nv) {
+		for(int node=1;node<=nv;node++) {
+			distance[node]=999;
+		}
+		distance[source]=0;
+		for(int node=1;node<=nv;node++) {
+			for(int sn=1;sn<=nv;sn++) {
+				for(int dn=1;dn<=nv;dn++) {
+					if(adj_matrix[sn][dn]!=999) {
+						if(distance[dn]>(distance[sn]+adj_matrix[sn][dn])) {
+							distance[dn]=distance[sn]+adj_matrix[sn][dn];
+						}
+					}
+				}
+			}
+		}
+		for( int sn=1;sn<=nv;sn++) {
+			for(int dn=1;dn<=nv;dn++) {
+				if(adj_matrix[sn][dn]!=999) {
+					if(distance[dn]>(distance[sn]+adj_matrix[sn][dn])) {	
+						System.out.println("graph contains negative edge cycles");
+						break;
+					}
+				}
+			}
+			
+		}
+		for(int ver=1;ver<=nv;ver++) {
+			System.out.println("distance from source "+source+" to destination "+ver+" is :"+distance[ver]);
+		}
+	}
+}
